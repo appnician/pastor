89f0ef24a1d2ad2d7c416a97d48858626c21eba6
Ashin Chen <ashin.chen@bosondata.com.cn>
2014-07-18 10:10:09 +0800

    set password to clip

--- pastor.py
+++ pastor.py
@@ -3,27 +3,29 @@
 import os
 import hashlib
 import getpass
+import pyperclip
 
-password_length = 12 - 5
+password_length = 16
 
-base_phrase = getpass.getpass( "Enter base phrase: " )
+base_phrase = getpass.getpass("Enter base phrase: ")
 
 sanity_check = hashlib.sha256()
-sanity_check.update( base_phrase )
+sanity_check.update(base_phrase)
 
-print sum( [ ord( x ) for x in sanity_check.digest() ] )
+print sum([ord(x) for x in sanity_check.digest()])
 
-valid_characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+valid_characters = 'abcdefghijklmnopqrstuvwxyz0123456789.~!@#$%^&*()_+'
 
-while ( True ) :
-  door_id = raw_input( "Enter door id: " )
-  if ( door_id == '' ) :
-    break
+while (True):
+    door_id = raw_input("Enter door id: ")
+    if (door_id == ''):
+        break
 
-  key_data = hashlib.sha256()
-  key_data.update( base_phrase + ' - ' + door_id )
+    key_data = hashlib.sha256()
+    key_data.update(base_phrase + ' - ' + door_id)
 
-  print ''.join( [ valid_characters[ ord( x ) % len( valid_characters ) ] for x in key_data.digest() ][:password_length] )
+    password = ''.join([valid_characters[ord(x) % len(valid_characters)] for x in key_data.digest()][:password_length])
+    pyperclip.setcb(password)
+    print pyperclip.getcb()
 
-os.system( 'clear' )
-
+os.system('clear')
